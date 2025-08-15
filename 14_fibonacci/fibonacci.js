c2ddb2fc2752415a6ba1f58a3524278bbcdd53c0
Your Name <you@example.com>
2025-08-15 22:24:07 +0200

    finished the last exercices

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(nb) {
+    if (nb < 0){
+        return "OOPS"
+    }
+
+    if (nb == 0){
+        return 0;
+    }
+    if (nb == 1){
+        return 1;
+    }
+    return fibonacci(nb-1) + fibonacci(nb-2);
 
 };
 
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+function getAge(birth, death){
 
-};
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth
+}
+
+const findTheOldest = function(arr) {
+
+    let oldest = arr[0];
+    for (const objects of arr) {
+        if(getAge(objects.yearOfBirth, objects.yearOfDeath) > getAge(oldest.yearOfBirth, oldest.yearOfDeath)){
+            oldest = objects;
+        }
+    }
+    return oldest;
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
